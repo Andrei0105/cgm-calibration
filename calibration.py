7eef3b980515849434ab67cbe4e120e3b59af7e6
Tudor-Andrei Vrabie <andrei0105@gmail.com>
2020-06-05 13:54:25 +0100

    Minor refactor

--- calibration.py
+++ calibration.py
@@ -1,17 +1,30 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from dataclasses import dataclass
+
+@dataclass
+class Point:
+    raw: float
+    glucose: float
+
+def get_slope_and_intercept_two_points(points):
+    # a list of two Points
+    slope = int((points[0].raw - points[1].raw) / (points[0].glucose - points[1].glucose))
+    intercept = int(points[0].raw - points[0].glucose * slope)
+    return slope, intercept
+
+def get_raw(slope, intercept, glucose):
+    return slope * glucose + intercept
+
 x = np.linspace(0,500,500)
-raw = [157412, 97059]
-glucose = [148, 90]
-# raw = [157412, ]
-# glucose = [148, ]
-plt.plot(glucose, raw, marker='x', color='red')
-slope = int((raw[0] - raw[1]) / (glucose[0] - glucose[1]))
-intercept = int(raw[1] - glucose[1]*slope)
-slopes_and_intercepts = [ (1004, 8331), (1237, 4296), (1658, -25469), (702, 46868), (970, 13481), (slope, intercept) ]
-# slopes_and_intercepts = [ (1004, 8331), (970, 13481), (1081, 32261), (1714, -76700), (823, 35215) ]
+
+points = [Point(157412, 148), Point(97059, 90)]
+slope, intercept = get_slope_and_intercept_two_points(points)
+plt.plot(x, slope * x + intercept, '-r', label='y=' + str(slope) + 'x + ' + str(intercept), c='red')
+
+slopes_and_intercepts = [ (1004, 8331), (1237, 4296), (1658, -25469), (702, 46868), (970, 13481)]
 for si in slopes_and_intercepts:
-    y = si[0]*x + si[1]
+    y = get_raw(si[0], si[1], x)
     plt.plot(x, y, '-r', label='y=' + str(si[0]) + 'x + ' + str(si[1]), c=np.random.rand(3,))
 plt.title('Calibration graph')
 plt.xlabel('mg/dl', color='#1C2833')
@@ -20,34 +33,14 @@ plt.legend(loc='upper left')
 plt.grid()
 plt.show()
 
-
-# mongo queries
-
-# {
-#     "dateString": {
-#         "$regex": "2020-06-03T10:5"
-#     }
-# }
-
-# {
-#    "glucoseType": "Finger"
-# }
-
-
-# 111 129765
-# 90 97059
-# 90 99 99765
-
-# raw = glucose * slope + intercept
-# glucose = (raw - intercept) / slope
-
 ref_slope = 1004
 ref_intercept = 8331
 glucose_values = [55, 70, 100, 130, 150, 180, 200, 240]
-raw_values = [gv * ref_slope + ref_intercept  for gv in glucose_values]
-slopes_and_intercepts = [ (970, 13481), (1081, 32261), (1714, -76700), (823, 35215) ]
+raw_values = [get_raw(ref_slope, ref_intercept, gv) for gv in glucose_values]
+slopes_and_intercepts = [ (970, 13481), (1081, 32261), (1714, -76700), (823, 35215), (1175, -16867) ]
 print(ref_slope, ref_intercept)
 print(glucose_values)
 for si in slopes_and_intercepts:
     print(si[0], si[1])
     print([int((rv - si[1]) / si[0]) for rv in raw_values])
+
