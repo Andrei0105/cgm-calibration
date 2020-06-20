990e2bb49808b7869634ddc193290a6c2b09c53a
Tudor-Andrei Vrabie <andrei0105@gmail.com>
2020-06-20 01:38:42 +0100

    Add inputs for nightscout api and token for api/v3

--- cgm-calibration-react/src/App.tsx
+++ cgm-calibration-react/src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, FormEvent } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+type AppState = {
+  tempNsUrl: string;
+  tempToken: string;
+  nightscoutUrl: string;
+  token: string;
+};
+
+export class App extends Component<{}, AppState> {
+  constructor(props: AppState) {
+    super(props);
+    this.updateNightscoutData = this.updateNightscoutData.bind(this);
+    this.updateTempNsUrl = this.updateTempNsUrl.bind(this);
+    this.updateTempToken = this.updateTempToken.bind(this);
+    this.state = { nightscoutUrl: "", tempNsUrl: "", token: "", tempToken: "" };
+  }
+
+  updateNightscoutData(e: FormEvent) {
+    e.preventDefault(); //prevent reload
+    this.setState({
+      nightscoutUrl: this.state.tempNsUrl,
+      token: this.state.tempToken,
+    });
+  }
+
+  updateTempNsUrl(e: FormEvent) {
+    this.setState({ tempNsUrl: (e.target as HTMLTextAreaElement).value });
+  }
+
+  updateTempToken(e: FormEvent) {
+    this.setState({ tempToken: (e.target as HTMLTextAreaElement).value });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>CGM calibration</h3>
+        <form onSubmit={this.updateNightscoutData}>
+          <label htmlFor="nightscout-input">Enter your Nightscout URL</label>
+          <input
+            id="nightscout-input"
+            onChange={this.updateTempNsUrl}
+            value={this.state.tempNsUrl}
+          />
+          <label htmlFor="token-input">Enter your token</label>
+          <input
+            id="token-input"
+            onChange={this.updateTempToken}
+            value={this.state.tempToken}
+          />
+          <button>Submit</button>
+        </form>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          The URL is {this.state.nightscoutUrl}, and the token is{" "}
+          {this.state.token}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
